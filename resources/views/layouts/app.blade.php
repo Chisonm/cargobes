6c18ebe140874ba46a18beaf15b82ff28888219c
Chisonm <37384220+Chisonm@users.noreply.github.com>
2020-11-02 13:53:51 +0100

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
            <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>@yield('title')  Unitednation Airwaybill</title>
+        <title>@yield('title')  Cargobes</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="manifest" href="site.webmanifest">
